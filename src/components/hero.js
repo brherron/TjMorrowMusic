72d8f967d9fbaae2ea6947c271cf0080af791847
Beau Herron <beauh@tkc32m.com>
2021-02-25 12:51:06 -0600

    Workin on CSS.

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { useScroll } from '../aux/useScroll.js'
 import { useSwipeable } from 'react-swipeable'
-
-import '../styles/hero.scss'
+import HeroImage from '../images/mainImg.jpg'
 
 const Hero = () => {
   const [ leftAlbumActive, setLeftAlbumActive] = useState(false);
@@ -26,29 +25,34 @@ const Hero = () => {
   return (
     <div id="music" className="hero">
       <div className="container">
-        <div className="left-container">
-          <div className="main">
-            <div className="main-img" style={{backgroundPositionY: (scrollY/40+70)+'%'}}></div>
-            <div className="bio-text">
-              Tj Morrow first picked up a guitar while in college and he hasn't looked back since. The midwest-based artist has performed in many bands throughout his 10 years of playing music. In 2019 he released his debut EP as a solo country artist.
-            </div>
-         </div>
-         <div className="main-text" style={{bottom: (scrollY/30+40)+'%'}}>
-         </div>
-        </div>
-        <div className="right-container">
-          <div className="main" style={{left: !leftAlbumActive ? "-5%" : "-20%"}}>
-            <div {...swipeHandlers} className="album-frame">
-              <div className="album-slider" style={{left: !leftAlbumActive ? "-80%" : "0%"}}>
-                <div role="button" tabIndex={0} className={leftAlbumActive ? "album1 active-album" : "album1 inactive-album"} onClick={clickAlbum} onKeyDown={clickAlbum} ></div>
-                <div role="button" tabIndex={0} className={leftAlbumActive ? "album2 inactive-album" : "album2 active-album"} onClick={clickAlbum} onKeyDown={clickAlbum}></div>
+        <div className="columns is-centered">
+          <div className="column left-container">
+            <div className="main">
+              {/* <div className="main-img" style={{backgroundPositionY: (scrollY/40+70)+'%'}}></div> */}
+              <figure className="image is-square">
+                <img className="main-img" src={HeroImage} alt=""/>
+              </figure>
+              <div className="bio-text">
+                Tj Morrow first picked up a guitar while in college and he hasn't looked back since. The midwest-based artist has performed in many bands throughout his 10 years of playing music. In 2019 he released his debut EP as a solo country artist.
               </div>
-              <div className="album-text">
-                <p className={!leftAlbumActive ? "hidden-text" : "visible-text"}>Live and Learn is the 2019 debut EP for country artist Tj Morrow.</p>
-                <p className={leftAlbumActive ? "hidden-text" : "visible-text"}>Released in August 2020, Dive Bar is the latest single from singer/songwriter Tj Morrow.</p>
-                <br />
-                <br />
-                <a className="listen-now" target="_blank" rel="noopener noreferrer" href={!leftAlbumActive ? "http://hyperurl.co/LittleDiveBar" : "http://hyperurl.co/LiveAndLearn"}>GET IT HERE</a>
+          </div>
+          <div className="main-text" style={{bottom: (scrollY/30+40)+'%'}}>
+          </div>
+          </div>
+          <div className="column right-container">
+            <div className="main" style={{left: !leftAlbumActive ? "-5%" : "-20%"}}>
+              <div {...swipeHandlers} className="album-frame">
+                <div className="album-slider" style={{left: !leftAlbumActive ? "-80%" : "0%"}}>
+                  <div role="button" tabIndex={0} className={leftAlbumActive ? "album1 active-album" : "album1 inactive-album"} onClick={clickAlbum} onKeyDown={clickAlbum} ></div>
+                  <div role="button" tabIndex={0} className={leftAlbumActive ? "album2 inactive-album" : "album2 active-album"} onClick={clickAlbum} onKeyDown={clickAlbum}></div>
+                </div>
+                <div className="album-text">
+                  <p className={!leftAlbumActive ? "hidden-text" : "visible-text"}>Live and Learn is the 2019 debut EP for country artist Tj Morrow.</p>
+                  <p className={leftAlbumActive ? "hidden-text" : "visible-text"}>Released in August 2020, Dive Bar is the latest single from singer/songwriter Tj Morrow.</p>
+                  <br />
+                  <br />
+                  <a className="listen-now" target="_blank" rel="noopener noreferrer" href={!leftAlbumActive ? "http://hyperurl.co/LittleDiveBar" : "http://hyperurl.co/LiveAndLearn"}>GET IT HERE</a>
+                </div>
               </div>
             </div>
           </div>
